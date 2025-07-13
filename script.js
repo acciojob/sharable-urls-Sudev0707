b709c47940fae2b5561218c629c19947c806a354
SUDEV MAJHI <58890958+Sudev0707@users.noreply.github.com>
2025-07-14 01:28:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-// your code here
+
+    document.getElementById("button").addEventListener("click", () => {
+      const name = document.getElementById("name").value.trim();
+      const year = document.getElementById("year").value.trim();
+      let url = "https://localhost:8080/";
+
+      const params = new URLSearchParams();
+
+      if (name !== "") params.append("name", name);
+      if (year !== "") params.append("year", year);
+
+      if ([...params].length > 0) {
+        url += "?" + params.toString();
+      }
+
+      document.getElementById("url").textContent = url;
+    });
+ 
